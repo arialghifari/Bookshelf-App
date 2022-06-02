7e1ce7c7d0264aba4c170ff094c652a1e422cf73
arialghifari <arialghifari99@gmail.com>
2022-06-02 17:09:02 +0800

    Edit & Update

--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ const isCompleted = document.getElementById("isCompleted");
 const btnSubmitText = document.getElementById("submit__status");
 const btnSubmit = document.getElementById("submit");
 const submitForm = document.getElementById("add");
-const btnReads = document.querySelectorAll("#btn-read");
-const btnUnreads = document.querySelectorAll("#btn-unread");
+const btnReads = document.getElementById("btn-read");
+const btnUnreads = document.getElementById("btn-unread");
+const judulInput = document.getElementById("judul-input");
+const penulisInput = document.getElementById("penulis-input");
+const tahunInput = document.getElementById("tahun-input");
+const newBookTitle = document.getElementById("new-book__title");
+let bookEl;
 
 document.addEventListener("DOMContentLoaded", function () {
   if (typeof Storage === undefined) {
@@ -16,45 +21,74 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   buttonActivity();
+  search();
+  isChecked();
   loadDataFromStorage();
   refreshDataFromBooks();
 });
 
-isCompleted.addEventListener("click", function () {
-  if (isCompleted.checked) {
-    btnSubmitText.innerText = "Sudah Selesai Dibaca";
-    btnSubmit.style.color = "#21ac56";
-  } else {
-    btnSubmitText.innerText = "Belum Selesai Dibaca";
-    btnSubmit.style.color = "#e66a44";
-  }
-});
+function isChecked() {
+  isCompleted.addEventListener("click", function () {
+    if (newBookTitle.innerText === "TAMBAH BUKU") {
+      btnSubmit.innerHTML = "Tambah ";
+    } else {
+      btnSubmit.innerHTML = "Update ";
+    }
+
+    if (isCompleted.checked) {
+      btnSubmit.innerHTML +=
+        'Buku Ke Rak <b id="submit__status">Sudah Selesai Dibaca</b>';
+      btnSubmit.style.color = "#21ac56";
+    } else {
+      btnSubmit.innerHTML +=
+        'Buku Ke Rak <b id="submit__status">Belum Selesai Dibaca</b>';
+      btnSubmit.style.color = "#e66a44";
+    }
+  });
+}
 
 submitForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (isCompleted.checked) {
-    addBook(true);
+  if (btnSubmit.classList.length > 0) {
+    isCompleted.checked
+      ? addBook(true, btnSubmit.classList[0])
+      : addBook(false, btnSubmit.classList[0]);
   } else {
-    addBook(false);
+    isCompleted.checked ? addBook(true) : addBook(false);
   }
 });
 
 function addBook(isCompleted) {
-  const judulInput = document.getElementById("judul-input").value;
-  const penulisInput = document.getElementById("penulis-input").value;
-  const tahunInput = document.getElementById("tahun-input").value;
+  if (bookEl) {
+    const btnCancel = document.getElementById("cancel");
+    const isChecked = document.getElementById("isCompleted");
+
+    btnCancel.classList.add("hidden");
+    isChecked.checked = false;
+    newBookTitle.innerHTML = "TAMBAH BUKU";
+
+    btnSubmit.innerHTML =
+      'Tambah Buku Ke Rak <b id="submit__status">Belum Selesai Dibaca</b>';
+    btnSubmit.style.color = "#e66a44";
+
+    const bookPosition = findBookIndex(bookEl[BOOK_ITEM_ID]);
+
+    books.splice(bookPosition, 1);
+    bookEl.remove();
+  }
 
   const book = makeBookElement(
-    judulInput,
-    penulisInput,
-    tahunInput,
+    judulInput.value,
+    penulisInput.value,
+    tahunInput.value,
     isCompleted
   );
+
   const bookObject = createBookObject(
-    judulInput,
-    penulisInput,
-    tahunInput,
+    judulInput.value,
+    penulisInput.value,
+    tahunInput.value,
     isCompleted
   );
 
@@ -69,40 +103,31 @@ function addBook(isCompleted) {
     listUndoneReading.append(book);
   }
 
-  clearInput();
-  updateDataToStorage();
-  refreshDataFromBooks();
+  refresh();
 }
 
 function makeBookElement(judulInput, penulisInput, tahunInput, isCompleted) {
   const bookContainer = document.createElement("div");
 
   if (isCompleted) {
-    bookDetailHTML = `
-			<div class="detail">
-				<h3 id="judul">${judulInput}</span></h3>
-				<p>Penulis: <span id="penulis">${penulisInput}</span></p>
-				<p>Tahun: <span id="tahun">${tahunInput}</span></p>
-			</div>
-			<div class="action">
-				<button id="btn-unread">Belum Dibaca</button>
-				<button id="btn-edit"></button>
-				<button id="btn-delete"></button>
-			</div>
-		`;
+    bookDetailHTML = makeBookDetailHTML("btn-unread", "Belum Dibaca");
   } else {
-    bookDetailHTML = `
-			<div class="detail">
-				<h3 id="judul">${judulInput}</h3>
-				<p>Penulis: <span id="penulis">${penulisInput}</span></p>
-				<p>Tahun: <span id="tahun">${tahunInput}</span></p>
-			</div>
-			<div class="action">
-				<button id="btn-read">Sudah Dibaca</button>
-				<button id="btn-edit"></button>
-				<button id="btn-delete"></button>
-			</div>
-		`;
+    bookDetailHTML = makeBookDetailHTML("btn-read", "Sudah Dibaca");
+  }
+
+  function makeBookDetailHTML(btn, text) {
+    return `
+      <div class="detail">
+        <h3 id="judul">${judulInput}</span></h3>
+        <p>Penulis: <span id="penulis">${penulisInput}</span></p>
+        <p>Tahun: <span id="tahun">${tahunInput}</span></p>
+      </div>
+      <div class="action">
+        <button id="${btn}">${text}</button>
+        <button id="btn-edit"></button>
+        <button id="btn-delete"></button>
+      </div>
+    `;
   }
 
   bookContainer.classList.add("book");
@@ -112,92 +137,56 @@ function makeBookElement(judulInput, penulisInput, tahunInput, isCompleted) {
 }
 
 function buttonActivity() {
-  document.addEventListener("click", function (e) {
-    const btnDeleteClicked = e.target.id == "btn-delete";
-    const btnReadClicked = e.target.id == "btn-read";
-    const btnUnreadClicked = e.target.id == "btn-unread";
-
-    if (btnDeleteClicked) {
-      const bookTitle =
-        e.target.parentElement.parentElement.querySelector("#judul").innerText;
-
-      Swal.fire({
-        title: `Yakin ingin menghapus buku "${bookTitle}"?`,
-        text: "Jika dihapus, data buku tidak bisa dikembalikan!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Iya, hapus!",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire("Terhapus!", "Buku sudah berhasil dihapus", "success");
-
-          const bookElement = e.target.parentElement.parentElement;
-          const bookPosition = findBookIndex(bookElement[BOOK_ITEM_ID]);
-
-          books.splice(bookPosition, 1);
-          bookElement.remove();
-
-          clearInput();
-          updateDataToStorage();
-          refreshDataFromBooks();
-        }
-      });
-    } else if (btnReadClicked) {
+  document
+    .querySelector(".book-section")
+    .addEventListener("click", function (e) {
       const bookElement = e.target.parentElement.parentElement;
-      const bookTitle =
-        e.target.parentElement.parentElement.querySelector("#judul").innerText;
-      const bookAuthor =
-        e.target.parentElement.parentElement.querySelector(
-          "#penulis"
-        ).innerText;
-      const bookYear =
-        e.target.parentElement.parentElement.querySelector("#tahun").innerText;
 
-      const newBook = makeBookElement(bookTitle, bookAuthor, bookYear, true);
-      const book = findBook(bookElement[BOOK_ITEM_ID]);
+      switch (e.target.id) {
+        case "btn-delete":
+          deleteBook(bookElement);
+          break;
+        case "btn-read":
+          swapBook(bookElement, true);
+          break;
+        case "btn-unread":
+          swapBook(bookElement, false);
+          break;
+        case "btn-edit":
+          setUpdateForm(bookElement);
+          break;
+      }
+    });
+}
 
-      book.isCompleted = true;
-      newBook[BOOK_ITEM_ID] = book.id;
+function deleteBook(bookElement) {
+  const bookTitle = bookElement.querySelector("#judul").innerText;
 
-      const listDoneReading = document.getElementById(READ_LIST_BOOK_ID);
-      listDoneReading.append(newBook);
+  Swal.fire({
+    title: `Yakin ingin menghapus buku "${bookTitle}"?`,
+    text: "Jika dihapus, data buku ini tidak bisa dikembalikan!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Iya, hapus!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Terhapus!", "Buku sudah berhasil dihapus", "success");
 
+      const bookPosition = findBookIndex(bookElement[BOOK_ITEM_ID]);
+
+      books.splice(bookPosition, 1);
       bookElement.remove();
 
-      clearInput();
-      updateDataToStorage();
-      refreshDataFromBooks();
-    } else if (btnUnreadClicked) {
-      const bookElement = e.target.parentElement.parentElement;
-      const bookTitle =
-        e.target.parentElement.parentElement.querySelector("#judul").innerText;
-      const bookAuthor =
-        e.target.parentElement.parentElement.querySelector(
-          "#penulis"
-        ).innerText;
-      const bookYear =
-        e.target.parentElement.parentElement.querySelector("#tahun").innerText;
-
-      const newBook = makeBookElement(bookTitle, bookAuthor, bookYear, false);
-      const book = findBook(bookElement[BOOK_ITEM_ID]);
-
-      book.isCompleted = false;
-      newBook[BOOK_ITEM_ID] = book.id;
-
-      const listUndoneReading = document.getElementById(UNREAD_LIST_BOOK_ID);
-      listUndoneReading.append(newBook);
-
-      bookElement.remove();
-
-      clearInput();
-      updateDataToStorage();
-      refreshDataFromBooks();
+      refresh();
     }
   });
+}
 
+function search() {
   const searchInput = document.getElementById("search-input");
+
   searchInput.addEventListener("keyup", function () {
     if (searchInput.value) {
       searchBook(searchInput.value);
@@ -205,6 +194,75 @@ function buttonActivity() {
       refreshDataFromBooks();
     }
   });
+}
+
+function swapBook(bookElement, value) {
+  const bookTitle = bookElement.querySelector("#judul").innerText;
+  const bookAuthor = bookElement.querySelector("#penulis").innerText;
+  const bookYear = bookElement.querySelector("#tahun").innerText;
+
+  const newBook = makeBookElement(bookTitle, bookAuthor, bookYear, value);
+  const book = findBook(bookElement[BOOK_ITEM_ID]);
+
+  book.isCompleted = value;
+  newBook[BOOK_ITEM_ID] = book.id;
+
+  const listDoneReading = document.getElementById(READ_LIST_BOOK_ID);
+  listDoneReading.append(newBook);
+
+  bookElement.remove();
+
+  refresh();
+}
+
+function setUpdateForm(bookElement) {
+  const judul = bookElement.children[0].children[0].innerHTML;
+  const penulis = bookElement.children[0].children[1].children[0].innerHTML;
+  const tahun = bookElement.children[0].children[2].children[0].innerHTML;
+  const status = bookElement.parentElement.id;
+  const btnCancel = document.getElementById("cancel");
+
+  window.scrollTo(0, 0);
+  judulInput.focus();
+
+  judulInput.value = judul;
+  penulisInput.value = penulis;
+  tahunInput.value = tahun;
+
+  if (status === "done-reading") {
+    isCompleted.checked = true;
+    btnSubmit.innerHTML =
+      'Update Buku Ke Rak <b id="submit__status">Sudah Selesai Dibaca</b>';
+    btnSubmit.style.color = "#21ac56";
+  } else {
+    isCompleted.checked = false;
+    btnSubmit.innerHTML =
+      'Update Buku Ke Rak <b id="submit__status">Belum Selesai Dibaca</b>';
+    btnSubmit.style.color = "#e66a44";
+  }
+
+  newBookTitle.innerHTML = "UPDATE BUKU";
+  btnSubmit.className = "";
+  btnCancel.classList.remove("hidden");
+  bookEl = bookElement;
+
+  btnCancel.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+    newBookTitle.innerHTML = "TAMBAH BUKU";
+    btnSubmit.innerHTML =
+      'Tambah Buku Ke Rak <b id="submit__status">Belum Selesai Dibaca</b>';
+    btnSubmit.style.color = "#e66a44";
+    btnCancel.classList.add("hidden");
+    isCompleted.checked = false;
+    refresh();
+  });
+}
+
+function refresh() {
+  clearInput();
+  updateDataToStorage();
+  refreshDataFromBooks();
 }
 
 function clearInput() {
